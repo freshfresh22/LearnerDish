8b7ec8555d9c3086f7a38989ca19d6ff4ab5b947
싱싱이 <03selee@naver.com>
2025-04-15 14:01:41 +0900

    Initial Commit

---
+++ LearnerDishTests/LearnerDishTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnerDishTests.swift
+//  LearnerDishTests
+//
+//  Created by 이시은 on 4/15/25.
+//
+
+import Testing
+@testable import LearnerDish
+
+struct LearnerDishTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
